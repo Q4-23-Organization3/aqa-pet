a3d8528e76e832501ab1f9ac7fc13d052d97d79a
stetsiukalla <neskazhinskaya@gmail.com>
2023-12-27 13:45:59 +0200

    Сommit to branch to make conflict

--- src/main/java/hw4/Task1.java
+++ src/main/java/hw4/Task1.java
@@ -3,5 +3,6 @@ package hw4;
 public class Task1 {
     public static void main(String[] args) {
         System.out.println("Довільний текст");
+        System.out.println("Довільний текст для створення конфлікту на новій гілці");
     }
 }
