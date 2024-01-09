d1f334a778718a6ecde83d60b9ea8c700d395a6c
stetsiukalla <neskazhinskaya@gmail.com>
2024-01-09 22:59:06 +0200

    ДЗ 8. ООП. Інкапсуляція

---
+++ src/main/java/hw8/task2/Main.java
@@ -0,0 +1,10 @@
+package hw8.task2;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator.summation(1, 3);
+        Calculator.subtraction(1, 3);
+        Calculator.multiplication(1, 3);
+        Calculator.division(6, 3);
+    }
+}
